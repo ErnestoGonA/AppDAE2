6e16d156c527010a8cccc753e2d6ddf02fb9998e
ernesto gonzalez arellano <ergonzalezar@ittepic.edu.mx>
2018-11-12 08:02:42 -0700

    FEAT: Creacion del proyecto

---
+++ AppDAE2/Areas/General/Controllers/FicCatEdificiosController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using AppDAE2.Areas.General.Services;
+
+namespace AppDAE2.Areas.General.Controllers
+{
+    //Se tiene que llamar igual que la carpeta de view
+    public class FicCatEdificiosController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ AppDAE2/Areas/General/Services/FicSrvCatEdificiosList.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Net.Http.Headers;
+
+namespace AppDAE2.Areas.General.Services
+{
+    public class FicSrvCatEdificiosList
+    {
+
+        HttpClient client;
+
+        public FicSrvCatEdificiosList(HttpClient client)
+        {
+            this.client = client;
+            this.client.BaseAddress = new Uri("http://localhost:51777/");
+            this.client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        }
+
+
+
+    }
+}
